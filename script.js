8dbd8d81c794486daf96ca3a02010b674b914e8b
Joshua Laoli <joe.pixarts@gmail.com>
2023-07-18 19:24:41 +0700

    update 19.24

--- script.js
+++ script.js
@@ -32,6 +32,37 @@ const generatePassword = () => {
 
     for (let i = 0; i < passLength; i++) {
         let randomChar = staticPassword [Math.floor(Math.random() * staticPassword.length)];
-        
+        if (excludeDuplicate) {
+            !randomPassword.includes(randomChar) | randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
     };
+    passwordInput.value = randomPassword;
 }
+
+const updatePassIndicator = () => {
+    passwordIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider <= 16 ? "medium" : "strong";
+}
+
+const updateSlider = () => {
+    document.querySelector (".pass-length span").innerText = lengthSlider.value;
+    generatePassword ();
+    updatePassIndicator ();
+}
+updateSlider ();
+
+const copyPassword = () => {
+    navigator.clipboard.writeText(passwordInput.value);
+    copyIcon.innerText = "check";
+    copyIcon.style.color = "#4285f4";
+    setTimeout( () => {
+        copyIcon.innerText = "copy_all";
+        copyIcon.style.color = "#707070";
+    }, 1500)
+
+}
+
+copyIcon.addEventListener("click", copyPassword);
+lengthSlider.addEventListener ("input", updateSlider);
+generateBtn.addEventListener ("click", generatePassword);
